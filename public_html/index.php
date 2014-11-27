c805a561be3b95ab41e0364bf57bbb02e462764c
Hitomi Nagano <naganohitomi@realglobe.jp>
2014-11-27 11:48:30 +0900

    google code add

--- public_html/index.php
+++ public_html/index.php
@@ -38,7 +38,16 @@ if (preg_match('/Android/',$_SERVER['HTTP_USER_AGENT'])) {
 <script type="text/javascript" src="assets/js/lightbox.js"></script>
 <script type="text/javascript" src="assets/js/jquery.clickSubMenu.js"></script>
 <script type="text/javascript" src="assets/js/jquery.PageTop.js"></script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
+  ga('create', 'UA-24317094-19', 'auto');
+  ga('send', 'pageview');
+
+</script>
 </head>
 <body>
 <div id="wrap_all">
